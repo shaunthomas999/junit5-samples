24332d50dcf27ce11c52ff1946f1ed4a5686d745
Shaun Thomas <shaunthomas999@gmail.com>
2022-03-22 13:15:31 +0100

    Add reference for Finonacci number generation

--- junit5-features/src/main/java/com/shaunthomas999/junit5/comparison/Calculator.java
+++ junit5-features/src/main/java/com/shaunthomas999/junit5/comparison/Calculator.java
@@ -18,6 +18,10 @@ public class Calculator {
     return a / b;
   }
 
+  /**
+   * Ref: https://en.wikipedia.org/wiki/Fibonacci_number
+   * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+   */
   public int fibonacci(int n) {
     if (n == 0) {
       return 0;
